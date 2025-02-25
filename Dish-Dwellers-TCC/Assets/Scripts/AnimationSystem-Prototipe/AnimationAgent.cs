b10a09c2e786d523a69416e98a97b135f4b39668
Pedro de Lima Birchal (Lima) <birchalpedro@gmail.com>
2025-02-25 04:18:23 -0300

    Cena de teste para animação
    Cena placeholder para teste de animação criada. Shader de iluminação de sprites introduzido

---
+++ Dish-Dwellers-TCC/Assets/Scripts/AnimationSystem-Prototipe/AnimationAgent.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class AnimationAgent : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    public void FlipHorizontal(){}
+    public void FlipVertical(){}
+    public void Walk(){}
+    public void Stop(){}
+}

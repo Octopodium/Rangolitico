471f2f2079a82a997ff68a0d27c7ff75ad4e61e1
Pedro de Lima Birchal (Lima) <birchalpedro@gmail.com>
2025-02-25 21:07:22 -0300

    Animation test

--- Dish-Dwellers-TCC/Assets/Scripts/AnimationSystem-Prototipe/AnimationAgent.cs
+++ Dish-Dwellers-TCC/Assets/Scripts/AnimationSystem-Prototipe/AnimationAgent.cs
@@ -1,11 +1,47 @@
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AnimationAgent : MonoBehaviour
 {
+    [Header("Components")]
     [SerializeField] private Animator animator;
 
-    public void FlipHorizontal(){}
-    public void FlipVertical(){}
-    public void Walk(){}
-    public void Stop(){}
+    [Header("Game Information"), Space(10)]
+    public static readonly int WalkSpeed = Animator.StringToHash(nameof(WalkSpeed));
+    public static readonly int Falling = Animator.StringToHash(nameof(Falling));
+    public static readonly int Carrying = Animator.StringToHash(nameof(Carrying));
+    public static readonly int Throw = Animator.StringToHash(nameof(Throw));
+    public static readonly int Dead = Animator.StringToHash(nameof(Dead));
+    public static readonly int Damage = Animator.StringToHash(nameof(Damage));
+    
+    
+
+    private void Start(){
+        animator = GetComponent<Animator>();
+        for( int i = 0; i < animator.parameterCount; i++){
+            AnimatorControllerParameter parameter = animator.GetParameter(i);
+            
+            Debug.Log($"\t parameter: {parameter.name} \t id: {parameter.GetHashCode()}");
+        }
+    }
+
+    [Tooltip("Set moving animation in accordance with the speed value informed.")]
+    public void Move(Vector3 speed, float maxSpeed){
+
+        if(speed.x > 0){ // Turn character to the right
+            transform.localScale = Vector3.Scale(transform.localScale, Vector3.right);
+        }
+        else if(speed.x < 0){// Turn character to the left
+            transform.localScale = Vector3.Scale(transform.localScale, Vector3.left);
+        }
+        if(speed.z > 0){// Turn character to the front            
+        }
+        else if(speed.z < 0){ // Turn character to the back            
+        }
+
+        float relativeSpeed = ((speed.x * speed.x) + (speed.z * speed.z)) / (maxSpeed * maxSpeed);
+        animator.SetFloat(WalkSpeed, relativeSpeed);
+    }
 }

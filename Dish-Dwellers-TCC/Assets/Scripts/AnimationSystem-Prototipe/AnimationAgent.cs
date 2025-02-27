7a97b5f7129205051fb97f320b9a153e3bae41c5
Pedro de Lima Birchal (Lima) <birchalpedro@gmail.com>
2025-02-27 15:18:16 -0300

    Update sistema de animação

--- Dish-Dwellers-TCC/Assets/Scripts/AnimationSystem-Prototipe/AnimationAgent.cs
+++ Dish-Dwellers-TCC/Assets/Scripts/AnimationSystem-Prototipe/AnimationAgent.cs
@@ -1,55 +1,120 @@
-using System;
-using System.Collections.Generic;
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class AnimationAgent : MonoBehaviour
 {
+    #region Propriedades
+
     [Header("Components")]
     [SerializeField] private Animator animator;
 
     [Header("Game Information"), Space(10)]
-    public static readonly int WalkSpeed = Animator.StringToHash(nameof(WalkSpeed));
+    public static readonly int Walk = Animator.StringToHash(nameof(Walk));
     public static readonly int Falling = Animator.StringToHash(nameof(Falling));
-    public static readonly int Carrying = Animator.StringToHash(nameof(Carrying));
+    public static readonly int Carry = Animator.StringToHash(nameof(Carry));
     public static readonly int Throw = Animator.StringToHash(nameof(Throw));
     public static readonly int Dead = Animator.StringToHash(nameof(Dead));
     public static readonly int Damage = Animator.StringToHash(nameof(Damage));
-    public bool iddle, run , change;
-    
-    
+    public bool _inAciotn;
+    public bool inAciotn{
+        get{
+            return _inAciotn;
+        }
+        private set{
+            _inAciotn = value;
+        }
+    }
+    public bool busy{
+        get{
+            // Pega informação do estado atual do animator
+            int stateID = animator.GetCurrentAnimatorStateInfo(0).shortNameHash;
+
+            // Checa  se o estado atual é algum dos listados
+            if(stateID == Falling ||  stateID == Dead || stateID == Damage)
+                return true;
+            return false;
+        }
+    }
+
+    #endregion
 
     private void Start(){
         animator = GetComponent<Animator>();
+        Move(new Vector3(0,0,0));
     }
 
-    [Tooltip("Set moving animation in accordance with the speed value informed.")]
-    public void Move(Vector3 speed, float maxSpeed){
+    #region Métodos de animação
 
-        if(speed.x > 0){ // Turn character to the right
+    /// <summary>
+    /// Atribui a animação de andar correta ao jogador, baseada no Vetor de velocidade speed.
+    /// </summary>
+    /// <param name="speed"></param>
+    public void Move(Vector3 speed){
+
+        if(speed.x > 0){ // Vira para a esquerda
             transform.localScale = Vector3.Scale(transform.localScale, Vector3.right);
         }
-        else if(speed.x < 0){// Turn character to the left
+        else if(speed.x < 0){// Vira para a direita
             transform.localScale = Vector3.Scale(transform.localScale, Vector3.left);
         }
-        if(speed.z > 0){// Turn character to the front            
+        if(speed.z > 0){// Vira de costas
+            Debug.Log("Vira de costas");
         }
-        else if(speed.z < 0){ // Turn character to the back            
-        }
-
-        float relativeSpeed = ((speed.x * speed.x) + (speed.z * speed.z)) / (maxSpeed * maxSpeed);
-        animator.SetFloat(WalkSpeed, relativeSpeed);
-    }
-
-    private void Update(){
-        if(change){
-            float sinVal = Mathf.PingPong(Time.time, 1);
-            animator.SetFloat(WalkSpeed, sinVal);
-        }
-        else if(run){
-            animator.SetFloat(WalkSpeed, 1);
-        }
-        else if(iddle){
+        else if(speed.z < 0){ // Vira para a frente
+            Debug.Log("Vira de frente");
         }
     }
+
+    /// <summary>
+    /// Atribui a animação de andar correta ao jogador, baseada no Vetor de velocidade speed.
+    /// </summary>
+    /// <param name="speed"></param>
+    public void Move(Vector2 speed){
+        if(speed.x > 0){ // Vira para a direita
+            transform.localScale = Vector3.Scale(transform.localScale, Vector3.right);
+        }
+        else if(speed.x < 0){// Vira para a esquerda
+            transform.localScale = Vector3.Scale(transform.localScale, Vector3.left);
+        }
+        if(speed.y > 0){// Vira de costas      
+            Debug.Log("Vira de costas");
+        }
+        else if(speed.y < 0){ // Vira para a frente
+            Debug.Log("Vira de frente");
+        }
+
+        animator.SetBool(Walk, true);
+    }
+
+    /// <summary>
+    /// Toca a animação de carregar objeto do personagem. Permanece na pose de carregar enquanto ThrowObject não for chamado.
+    /// </summary>
+    public void PickUp(){
+        animator.SetTrigger(Carry);
+    }
+
+    /// <summary>
+    /// Toca a animação de arremesso do personagem.
+    /// </summary>
+    public void ThrowObject(){
+        animator.SetTrigger(Throw);
+    }
+
+    /// <summary>
+    /// Toca a animação de morte do personagem.
+    /// </summary>
+    public void Die(){
+        animator.SetTrigger(Dead);
+    }
+
+    /// <summary>
+    /// Toca a animação de receber dano do personagem.
+    /// </summary>
+    public void TakeDamage(){
+        animator.SetTrigger(Damage);
+    }
+
+    #endregion
 }

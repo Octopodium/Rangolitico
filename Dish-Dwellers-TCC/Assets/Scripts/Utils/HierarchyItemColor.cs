2d50437a9d701266f2cc4d58236ad2724e534313
anaclaragl <ag.linhares2@gmail.com>
2025-03-26 00:45:18 -0300

    Hierarquia com corzinhas

---
+++ Dish-Dwellers-TCC/Assets/Scripts/Utils/HierarchyItemColor.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEditor;
+ 
+[InitializeOnLoad]
+///<sumary>
+/// Codigo adaptado do video: https://youtu.be/H8j4CbnVTfQ?si=oilnJdqfuS9lf0yy
+///<sumary>
+public static class HierarchyItemColor
+{
+    static HierarchyItemColor()
+    {
+        EditorApplication.hierarchyWindowItemOnGUI += HierarcheWindowItemOnGUI;
+    }
+ 
+    static void HierarcheWindowItemOnGUI(int instanceID, Rect selectRect)
+    {
+        var gameobject = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+        if(gameobject != null && gameobject.name.StartsWith("#",System.StringComparison.Ordinal))
+        {
+            //Descomente se precisar de cores especificas para itens especificos
+
+            /*string nameText = gameobject.name.Substring(1);
+            if(nameText == "Player"){
+                EditorGUI.DrawRect(selectRect, new Color(0.2f, 0.3f, 0f, 1f));
+            }else if(nameText == "Camera"){
+                EditorGUI.DrawRect(selectRect, new Color(0f, 0.3f, 0.2f, 1f));
+            }else{*/
+                EditorGUI.DrawRect(selectRect, Color.black); //cor default
+            //}
+            EditorGUI.DropShadowLabel(selectRect, gameobject.name.Replace("#", "").ToUpperInvariant());
+        }
+    }
+}

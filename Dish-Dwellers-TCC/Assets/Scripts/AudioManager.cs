5cbb6cfd17b4ef206a528a9ac0089e2d7b41c60f
TiagoANeto <tiago98.ta@gmail.com>
2025-03-30 23:05:34 -0300

    Audio Manager configurado, música em todas as cenas

--- Dish-Dwellers-TCC/Assets/Scripts/AudioManager.cs
+++ Dish-Dwellers-TCC/Assets/Scripts/AudioManager.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private AudioSource audioSource;
+    public AudioSource musicaAtual;
+
+    void Awake()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = 0.03f;
     }
 
-    // Update is called once per frame
-    void Update()
+    //Previsão de Slider Para o futuro...
+    public void MusicSoundController(float value)
     {
-        
+        audioSource.volume = value;
     }
 }

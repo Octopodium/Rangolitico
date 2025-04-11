c7ce74c3ff3510cb1c0210012671edc7b6b04621
anaclaragl <ag.linhares2@gmail.com>
2025-04-11 14:52:32 -0300

    Funcionando ler o tipo de controle e trocando o prefab

--- Dish-Dwellers-TCC/Assets/Scripts/Interagivel/Interagivel.cs
+++ Dish-Dwellers-TCC/Assets/Scripts/Interagivel/Interagivel.cs
@@ -4,19 +4,12 @@ public class Interagivel : MonoBehaviour {
 
     [Header("Indicador de interação")]
     public GameObject indicadorPrefab;
-    GameObject indicador;
+    //GameObject indicador;
     public Vector3 offsetIndicador = Vector3.up;
 
 
     // Awake: coisas que são da própria classe
     void Awake() {
-        /*if (indicadorPrefab) {
-            indicador = Instantiate(indicadorPrefab, transform.position + offsetIndicador, Quaternion.identity);
-            indicador.transform.SetParent(transform);
-            indicador.SetActive(false);
-        } else {
-            Debug.LogWarning("Indicador de interação não configurado");
-        }*/
         indicadorPrefab = GameObject.Find("IndicadorE");
     }
 
--- Dish-Dwellers-TCC/Assets/Scripts/Utils/LeitorDeControle.cs
+++ Dish-Dwellers-TCC/Assets/Scripts/Utils/LeitorDeControle.cs
@@ -6,44 +6,33 @@ public class LeitorDeControle : MonoBehaviour
     //Esquemas de UI por controle
     public GameObject gamepadUI;
     public GameObject keyboardUI;
-    public GameObject prefabY, prefabE;
+    public GameObject prefabY, prefabE; //depois vou analisar a possibilidade de mudar pra 1 prefab e trocar so o sprite
     public Interagivel[] interagiveis;
-    public string currentControlScheme;
-    //public PlayerInput playerInput;
+
+    public Actions input;
 
     //pegar todos os interagiveis do jogo e mudar o prefab
-
     public void Start(){
         interagiveis = FindObjectsOfType(typeof(Interagivel)) as Interagivel[];
-        ChecaInput();
-        //playerInput = GetComponent<PlayerInput>();
-
+        input = GameManager.instance.input;
+        input.Player.Get().actionTriggered += ChecaInput; //pega qualquer acao triggerada
     }
 
-    public void Udpate(){
-        //currentControlScheme = playerInput.currentControlScheme;
-        //Debug.Log("Esquema atual: " + playerInput.currentControlScheme);
-        ChecaInput();
-    }
+    public void ChecaInput(InputAction.CallbackContext ctx){
+        InputDevice controle = ctx.control.device;
 
-    public void ChecaInput(){
-        //nao funciona meu deus ja tentei todo tipo de leitor aaaaa como pode um ANYKEY nao LER ANYKEY
-        bool isGamepad = Gamepad.current != null && (Gamepad.current.leftStick.ReadValue().magnitude > 0.1f || Gamepad.current.buttonSouth.isPressed);
-        bool isKeyboard = Keyboard.current != null && (Keyboard.current.anyKey.isPressed || Mouse.current.delta.ReadValue().magnitude > 0);
+        if(controle is Gamepad){
+            //para quando tiver indicacao de botoes na UI
+            //keyboardUI.SetActive(false); gamepadUI.SetActive(true);
 
-        if(isGamepad){
-            Debug.Log("gpad");
-            //keyboardUI.SetActive(false);
-            //gamepadUI.SetActive(true);
-            if(prefabE.activeSelf) prefabE.SetActive(false);
+            if(prefabE.activeSelf) prefabE.SetActive(false); //para impedir conflitos de multiple devices
 
             foreach(Interagivel interagivel in interagiveis){
                 interagivel.indicadorPrefab = prefabY;
             }
-        }else if(isKeyboard){
-            Debug.Log("ley");
-            //gamepadUI.SetActive(false);
-            //keyboardUI.SetActive(true);
+        }else if(controle is Keyboard){
+            //gamepadUI.SetActive(false); keyboardUI.SetActive(true);
+            
             if(prefabY.activeSelf) prefabY.SetActive(false);
 
             foreach(Interagivel interagivel in interagiveis){

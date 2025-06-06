f7b536de3558be5693dd063f3e89dab5118dd077
Pedro de Lima Birchal (Lima) <birchalpedro@gmail.com>
2025-06-06 19:00:19 -0300

    checkpoint

---
+++ Dish-Dwellers-TCC/Assets/Scripts/Salas/Checkpoint.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class Checkpoint : MonoBehaviour {
+    public Transform[] spawnPoints = new Transform[2];
+    [HideInInspector]public BoxCollider col;
+
+
+    private void Start() {
+        Setup();
+    }
+
+    private void OnValidate() {
+        Setup();
+    }
+
+    private void Setup() {
+        col = GetComponent<BoxCollider>();
+        col.isTrigger = true;
+
+        col.center = new Vector3(0, col.size.y / 2, 0);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            HabilitarCheckPoint();
+        }
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = new Color(0, 1, 1, 0.2f);
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.DrawCube(col.center, col.size);
+        
+        
+    }
+
+    private void HabilitarCheckPoint() {
+        sala sala = GameManager.instance.salaAtual;
+        sala.spawnPoints = spawnPoints;
+    }
+}
